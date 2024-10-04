9d2199ce91238a88a2abc7b96786b54ddfb168b5
habibium <habib.prodev@gmail.com>
2024-10-04 10:23:21 +0600

    feat: implement URL validation and for POST /api/shorturl

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+const dns = require("node:dns/promises");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.route("/api/shorturl").post(async (req, res) => {
+  const url = req.body?.url;
+
+  const errRes = (message) => ({ error: message });
+
+  try {
+    const parsedUrl = new URL(url);
+    await dns.lookup(parsedUrl.hostname);
+
+    res.redirect("/");
+  } catch (e) {
+    if (e instanceof TypeError) return res.json(errRes`Invalid URL`);
+    if (e?.code === "ENOTFOUND") return res.json(errRes`Invalid Hostname`);
+    return res.json(errRes`Unexpected Error`);
+  }
 });
 
 app.listen(port, function () {
